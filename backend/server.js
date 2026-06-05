405ff4bbed616823447f31926ff5e38aecf86cae
Miculin Sergiu <miculinsergiu-alt@users.noreply.github.com>
2026-06-05 21:02:15 +0300

    chore: update cors origin to frontend render url

--- backend/server.js
+++ backend/server.js
@@ -4,7 +4,7 @@ const cors = require('cors');
 require('dotenv').config();
 
 const app = express();
-app.use(cors());
+app.use(cors({ origin: 'https://cm2026flex2.onrender.com' }));
 app.use(express.json());
 
 const pool = new Pool({ 
